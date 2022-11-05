05e1dd1eb3c4671b01dcdda0123711abca2dbbb7
Ovella <oks.vella@yandex.ru>
2022-11-05 18:42:31 +0300

    added task 1 and diagram

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Enter some numbers(divided by space): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+countNumbers(array);
+Console.WriteLine($"Number of elements > 0: {count}");
+
+
+void countNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+}
